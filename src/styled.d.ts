aa73281a4f8ad3a59fb7f238069487d1d7420572
Mitzi <mitzisantos13@gmail.com>
2021-08-26 17:56:07 -0300

    Create a theme object with colors for each pokemon type and text

---
+++ src/styled.d.ts
@@ -0,0 +1,56 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      type: {
+        bug: string;
+        dark: string;
+        dragon: string;
+        electric: string;
+        fairy: string;
+        fighting: string;
+        fire: string;
+        flying: string;
+        ghost: string;
+        grass: string;
+        ground: string;
+        ice: string;
+        normal: string;
+        poison: string;
+        psychic: string;
+        rock: string;
+        steel: string;
+        water: string;
+      };
+      background: {
+        type: {
+          bug: string;
+          dark: string;
+          dragon: string;
+          electric: string;
+          fairy: string;
+          fighting: string;
+          fire: string;
+          flying: string;
+          ghost: string;
+          grass: string;
+          ground: string;
+          ice: string;
+          normal: string;
+          poison: string;
+          psychic: string;
+          rock: string;
+          steel: string;
+          water: string;
+        };
+      };
+      text: {
+        white: string;
+        black: string;
+        grey: string;
+        number: string;
+      };
+    };
+  }
+}
