fb4d955cf47112cbda055c2218195fc28957d543
Mitzi <mitzisantos13@gmail.com>
2021-08-27 17:05:47 -0300

    Add a container

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import styled from "styled-components";
 
 export const App = () => {
   return (
-    <>
+    <Container>
       <Title>Pokédex</Title>
       <PokemonList />
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 0 1rem;
+
+  @media (min-width: 23.4375rem) {
+    padding: 0 2.5rem;
+  }
+`;
 
 const Title = styled.h1`
   margin-top: 2.1875rem;
